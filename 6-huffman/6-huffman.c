bb04620cd14148ee634c943b23dfe815beccb159
qlsl1198 <162524733+qlsl1198@users.noreply.github.com>
2024-10-15 15:36:58 +0900

    Update 6-huffman.c

--- 6-huffman/6-huffman.c
+++ 6-huffman/6-huffman.c
@@ -8,15 +8,11 @@ typedef struct TreeNode {
     struct TreeNode *left, *right;
 } TreeNode;
 
-// 우선순위 큐를 관리할 노드 구조체
-typedef struct MinHeapNode {
-    TreeNode* treeNode;
-    struct MinHeapNode* next;
-} MinHeapNode;
-
-// 우선순위 큐의 헤더 구조체
+// 우선순위 큐(최소 힙)의 구조체 정의
 typedef struct MinHeap {
-    MinHeapNode* head;
+    int size;
+    int capacity;
+    TreeNode** array;
 } MinHeap;
 
 // 새로운 트리 노드를 생성하는 함수
@@ -28,64 +24,94 @@ TreeNode* createTreeNode(char character, int frequency) {
     return newNode;
 }
 
-// 새로운 우선순위 큐 노드를 생성하는 함수
-MinHeapNode* createMinHeapNode(TreeNode* treeNode) {
-    MinHeapNode* newNode = (MinHeapNode*)malloc(sizeof(MinHeapNode));
-    newNode->treeNode = treeNode;
-    newNode->next = NULL;
-    return newNode;
+// 우선순위 큐를 초기화하는 함수
+MinHeap* createMinHeap(int capacity) {
+    MinHeap* minHeap = (MinHeap*)malloc(sizeof(MinHeap));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (TreeNode**)malloc(capacity * sizeof(TreeNode*));
+    return minHeap;
 }
 
-// 우선순위 큐를 초기화하는 함수
-MinHeap* createMinHeap() {
-    MinHeap* minHeap = (MinHeap*)malloc(sizeof(MinHeap));
-    minHeap->head = NULL;
-    return minHeap;
+// 두 노드를 교환하는 함수
+void swap(TreeNode** a, TreeNode** b) {
+    TreeNode* temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 // 우선순위 큐 상태를 출력하는 함수
 void printMinHeap(MinHeap* minHeap) {
-    MinHeapNode* current = minHeap->head;
-    while (current != NULL) {
-        printf("%d ", current->treeNode->frequency);
-        current = current->next;
+    for (int i = 0; i < minHeap->size; i++) {
+        printf("%d ", minHeap->array[i]->frequency);
     }
     printf("\n");
 }
 
+// 힙 속성을 유지하며 노드를 위로 올리는 함수
+void heapifyUp(MinHeap* minHeap, int index) {
+    int parent = (index - 1) / 2;
+    while (index > 0 && minHeap->array[index]->frequency < minHeap->array[parent]->frequency) {
+        swap(&minHeap->array[index], &minHeap->array[parent]);
+        index = parent;
+        parent = (index - 1) / 2;
+    }
+}
+
 // 우선순위 큐에 노드를 삽입하는 함수
 void insertMinHeap(MinHeap* minHeap, TreeNode* treeNode) {
-    MinHeapNode* newNode = createMinHeapNode(treeNode);
-
-    if (!minHeap->head || treeNode->frequency < minHeap->head->treeNode->frequency) {
-        newNode->next = minHeap->head;
-        minHeap->head = newNode;
-    } else {
-        MinHeapNode* current = minHeap->head;
-        while (current->next && current->next->treeNode->frequency <= treeNode->frequency) {
-            current = current->next;
-        }
-        newNode->next = current->next;
-        current->next = newNode;
+    if (minHeap->size == minHeap->capacity) {
+        printf("Heap is full\n");
+        return;
     }
+
+    minHeap->size++;
+    int i = minHeap->size - 1;
+    minHeap->array[i] = treeNode;
+
+    heapifyUp(minHeap, i);
 
     // 삽입 후 현재 우선순위 큐 상태 출력
     printMinHeap(minHeap);
 }
 
+// 힙 속성을 유지하며 노드를 아래로 내리는 함수
+void heapifyDown(MinHeap* minHeap, int index) {
+    int smallest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->frequency < minHeap->array[smallest]->frequency)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->frequency < minHeap->array[smallest]->frequency)
+        smallest = right;
+
+    if (smallest != index) {
+        swap(&minHeap->array[index], &minHeap->array[smallest]);
+        heapifyDown(minHeap, smallest);
+    }
+}
+
 // 우선순위 큐에서 최소값을 제거하고 반환하는 함수
 TreeNode* extractMin(MinHeap* minHeap) {
-    if (!minHeap->head) return NULL;
-    MinHeapNode* temp = minHeap->head;
-    minHeap->head = minHeap->head->next;
-    TreeNode* treeNode = temp->treeNode;
-    free(temp);
-    return treeNode;
+    if (minHeap->size <= 0) return NULL;
+    if (minHeap->size == 1) {
+        minHeap->size--;
+        return minHeap->array[0];
+    }
+
+    TreeNode* root = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+    minHeap->size--;
+    heapifyDown(minHeap, 0);
+
+    return root;
 }
 
 // 허프만 트리를 생성하는 함수
 TreeNode* buildHuffmanTree(char characters[], int frequencies[], int size) {
-    MinHeap* minHeap = createMinHeap();
+    MinHeap* minHeap = createMinHeap(size);
 
     // 각 문자에 대해 트리 노드를 생성하고 우선순위 큐에 삽입
     for (int i = 0; i < size; i++) {
@@ -93,7 +119,7 @@ TreeNode* buildHuffmanTree(char characters[], int frequencies[], int size) {
     }
 
     // 큐에 하나의 노드가 남을 때까지 반복하여 허프만 트리 빌드
-    while (minHeap->head && minHeap->head->next) {
+    while (minHeap->size > 1) {
         // 가장 작은 두 개의 노드를 꺼냄
         TreeNode* left = extractMin(minHeap);
         TreeNode* right = extractMin(minHeap);
